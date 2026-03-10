457b40e2af2374ba099cc7a58032620f5a99cac1
Alain Perez <alain.perez@openbravo.com>
2026-03-10 13:15:48 -0400

    mostrar grupo en admin

--- presupuesto/admin.py
+++ presupuesto/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from .models import DriveFolder, SolicitudPresupuesto, Sede, CuentaAnalitica, GoogleConfig, Configuracion, AdjuntoSolicitud, CuentaContable
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_groups')
+    
+    @admin.display(description='Grupos')
+    def get_groups(self, obj):
+        return ", ".join([g.name for g in obj.groups.all()])
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 @admin.register(SolicitudPresupuesto)
 class SolicitudAdmin(admin.ModelAdmin):
