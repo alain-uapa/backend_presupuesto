7811ba83a4f322dfa9889685e44d2a3d6200c755
Alain Perez <alain.perez@openbravo.com>
2026-06-26 09:58:00 -0400

    Configurado el correo de presupuesto para envio de correos

--- emails/config.py
+++ emails/config.py
@@ -2,8 +2,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'alainperez@uapa.edu.do'
-EMAIL_HOST_PASSWORD = 'alqc rdyp gbzf wrbr' #Código de App password 
+EMAIL_HOST_USER = 'presupuesto@uapa.edu.do'
+EMAIL_HOST_PASSWORD = 'srud uvtn udpd fnkr' #Código de App password 
 DEFAULT_FROM_EMAIL = 'Solicitud de Presupuesto'
 
 # Imprimir en la terminal
